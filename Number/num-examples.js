5f6bd03b307ec733702d46745521350227c0041f
Qasim Naseer <qasim.naseer.314@gmail.com>
2025-08-02 20:26:08 +0000

    Number and Math basics but important  functions

---
+++ Number/num-examples.js
@@ -0,0 +1,18 @@
+///============================================Numbers
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString())
+console.log(balance.toString().length)
+console.log(balance.toFixed(2))
+
+
+const anoherNumber=12.12345670
+console.log(anoherNumber.toPrecision(3))
+console.log(anoherNumber.toPrecision(2))
+console.log(anoherNumber.toPrecision(1))
+
+ const hundereds=10000000
+console.log(hundereds.toLocaleString())
+console.log(hundereds.toLocaleString('en-in'))
