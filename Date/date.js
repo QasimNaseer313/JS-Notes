0b99398e28f63933849cf63803526e7fd8112c84
Qasim Naseer <qasim.naseer.314@gmail.com>
2025-08-03 08:55:21 +0000

    Date basic methods and commonly used

---
+++ Date/date.js
@@ -0,0 +1,11 @@
+let myDat=new Date()
+console.log(myDat)
+console.log(myDat.toString())
+console.log(myDat.toDateString())
+console.log(myDat.toISOString())
+console.log(myDat.toJSON())
+console.log(myDat.toLocaleDateString())
+console.log(myDat.toLocaleString())
+
+
+
